242d6173ad69d8f833c170c36c6b53fc4420694e
Nubthong Worathong <nubthongwor@gmail.com>
2023-10-15 19:20:51 +0700

    (+) add big-integer lib

--- script.js
+++ script.js
@@ -9,18 +9,21 @@ document.getElementById('derangement-form').addEventListener('submit', function(
     }
 
     let result = derangements(n);
-    document.getElementById('result').innerText = 'Result: ' + result;
+    document.getElementById('result').innerText = 'Result: ' + result.toString();
 });
 
 function derangements(n) {
     if (n <= 1) {
-        return 0;
+        return bigInt(0);
     }
-    let T = Array(n + 1).fill(0);
-    T[1] = 0;
-    T[2] = 1;
+    
+    let T = Array.from({ length: n + 1 }).map(() => bigInt(0));
+    T[1] = bigInt(0);
+    T[2] = bigInt(1);
+
     for (let i = 3; i <= n; i++) {
-        T[i] = (i - 1) * (T[i - 1] + T[i - 2]);
+        T[i] = bigInt(i - 1).multiply(T[i - 1].add(T[i - 2]));
     }
+
     return T[n];
 }
